47be5363f50c650483537e29ed7e260a0c6a68f3
Jared Menard <jared.d.menard@gmail.com>
2023-12-09 23:22:49 -0800

    working title screen and player naming

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pygame
 from title_screen import TitleScreen
 from game_manager import GameManager
 
+
 if __name__ == "__main__":
     logging_config.setup_logging()
     pygame.init()
@@ -10,27 +11,32 @@ if __name__ == "__main__":
     pygame.display.init()
     screen_info = pygame.display.Info()
     screen_width, screen_height = screen_info.current_w, screen_info.current_h
-    print(f"{screen_width}, {screen_height}")
-    screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+    screen = pygame.display.set_mode((1920, 1080)) #pygame.FULLSCREEN
     title_screen = TitleScreen(screen, screen_width, screen_height)
     game_manager = GameManager(screen, screen_width, screen_height)
     fade_in_done = False
     title_screen.init_music(0.75)
-    current_state = game_manager.current_state
-
+    current_state = "title_screen"
     while True:
+        events = pygame.event.get()
         if current_state == "title_screen":
+            for event in events:
+                if event.type == pygame.KEYDOWN:
+                    print(f"Keydown event detected in main loop, sent to title_screen to handle")
+                title_screen.handle_event(event)
+                
             title_screen.update()
             if title_screen.is_finished:
+                pygame.mixer.music.stop()
                 game_manager.player.name = title_screen.player_name
                 current_state = "fade_in"
 
         elif current_state == "fade_in" and not fade_in_done:
-            fade_surface = pygame.Surface((game_manager.screen_width, game_manager.screen_height))
+            fade_surface = pygame.Surface((screen_width, screen_height))
             fade_surface.fill((0, 0, 0))
-            for a in range(255, -1, -5):
+            for alpha in range(255, -1, -5):
                 game_manager.draw_initial_ui_onto_surface(screen)
-                fade_surface.set_alpha(a)
+                fade_surface.set_alpha(alpha)
                 screen.blit(fade_surface, (0, 0))
                 pygame.display.flip()
                 pygame.time.delay(35)
@@ -40,39 +46,36 @@ if __name__ == "__main__":
             pygame.mixer.music.set_volume(0.3)
 
         elif current_state == 'game_loop':
-            events = pygame.event.get()
             for event in events:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     exit()
-                if event.type == pygame.KEYDOWN:
+                elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
                         game_manager.toggle_fullscreen()
                     elif event.key == pygame.K_r:
                         game_manager.restart_game()
                     else:
                         game_manager.process_keypress(event)
-                if game_manager.check_for_combat():
-                    current_state = 'combat'
-                    pygame.mixer.music.stop()
-                    game_manager.combat.init_music(volume=0.7)
-                game_manager.ui.process_input(events)
-
-            if current_state != 'combat':
+                # Process other game loop specific events here
+            if not game_manager.check_for_combat():
                 game_manager.update()
                 pygame.display.flip()
+            else:
+                current_state = 'combat'
+                pygame.mixer.music.stop()
+                game_manager.combat.init_music(volume=0.7)
 
         elif current_state == 'combat':
             game_manager.combat.update()
             game_manager.update()
             events = pygame.event.get()
             game_manager.ui.process_input(events)
-            
             if game_manager.combat.is_over:
                 pygame.mixer.music.stop()
                 game_manager.ui.room_display.update_post_combat(screen)
                 game_manager.combat.is_over = False
-                
                 game_manager.combat.resume_regular_music()
                 current_state = 'game_loop'
-                        
+
+pygame.quit()
--- title_screen.py
+++ title_screen.py
@@ -12,92 +12,114 @@ class TitleScreen:
         self.font_large = pygame.font.Font(None, 80)
         self.font_small = pygame.font.Font(None, 30)
         self.is_finished = False
+        self.color_inactive = pygame.Color('lightskyblue3')
+        self.color_active = pygame.Color('dodgerblue2')
         self.width = width
         self.height = height
+        self.placeholder_text = 'PLAYER'
+        self.cursor_visible = False
+        self.cursor_timer = pygame.time.get_ticks() 
+        self.button_color = self.color_inactive 
+        self.active = True
+        self.text = "PLAYER"
 
     def init_music(self, volume):
         pygame.mixer.music.load('music/bgmusic.mp3')
         pygame.mixer.music.set_volume(volume)
-        pygame.mixer.music.play(-1)  # Loop the music
+        pygame.mixer.music.play(-1)
 
     def render_text(self):
         title_text = self.font_large.render("The Endless Anguish", True, (211, 211, 211))  # Very light gray color
-        title_rect = title_text.get_rect(center=(self.screen.get_width() / 2, self.screen.get_height() / 3))
-        
-        self.screen.blit(title_text, title_rect)
+        self.title_rect = title_text.get_rect(center=(self.screen.get_width() / 2, self.screen.get_height() / 3))
+        self.screen.blit(title_text, self.title_rect)
         border_color = (144, 238, 144)  # Light green border
-        pygame.draw.rect(self.screen, border_color, title_rect.inflate(20, 20), 2)
+        pygame.draw.rect(self.screen, border_color, self.title_rect.inflate(20, 20), 2)
         self.input_box = pygame.Rect(100, 150, 140, 32)
         self.button = pygame.Rect(100, 200, 100, 32)
-        self.color_inactive = pygame.Color('lightskyblue3')
-        self.color_active = pygame.Color('dodgerblue2')
         self.color = self.color_inactive
-        self.active = False
-        self.text = ''
         self.font_small = pygame.font.Font(None, 30)
 
-    def update(self):
-        self.screen.fill((0, 0, 0))
-        self.render_text()
-        self.position_ui_elements()
-        self.draw_label()
-        self.draw_input_box()
-        self.draw_button()
-        pygame.display.flip()
-
     def position_ui_elements(self):
-        title_rect = self.font_large.render("The Endless Anguish", True, (211, 211, 211)).get_rect(center=(self.screen.get_width() / 2, self.screen.get_height() / 3))
-        title_bottom = title_rect.bottom
-        # Horizontal center of the screen
+        title_bottom = self.title_rect.bottom
         center_x = self.screen.get_width() / 2
-        # Calculate width of each element
         input_box_width = self.input_box.width
         button_width = self.button.width
-        label_width = self.font_small.size("What is your name?")[0]  # Get the width of the label text
-        # Position the elements
+        label_width = self.font_small.size("What is your name?")[0]
         self.input_box.topleft = (center_x - input_box_width // 2, title_bottom + 80)
         self.button.topleft = (center_x - button_width // 2, title_bottom + 120)
         self.label_pos = (center_x - label_width // 2, title_bottom + 40)
-
-    def handle_event(self, event):
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if self.input_box.collidepoint(event.pos):
-                self.active = not self.active
-            else:
-                self.active = False
-            self.color = self.color_active if self.active else self.color_inactive
-
-            if self.button.collidepoint(event.pos) and not self.is_finished:
-                self.process_start_game()
-                return
-        if event.type == pygame.KEYDOWN:
-            if self.active:
-                if event.key == pygame.K_RETURN:
-                    self.process_start_game()
-                    return
-                elif event.key == pygame.K_BACKSPACE:
-                    self.text = self.text[:-1]
-                else:
-                    self.text += event.unicode
 
     def draw_label(self):
         label = self.font_small.render("What is your name?", True, (211, 211, 211))
         self.screen.blit(label, self.label_pos)
 
     def draw_input_box(self):
-        # Render the current text.
-        txt_surface = self.font_small.render(self.text, True, self.color)
-        # Resize the box if the text is too long.
+        current_text = self.text if self.active else self.placeholder_text
+        txt_color = self.color if self.active else pygame.Color('grey')  # Use grey color for placeholder
+        txt_surface = self.font_small.render(current_text, True, txt_color)
         width = max(200, txt_surface.get_width() + 10)
         self.input_box.w = width
-        # Blit the text.
+        box_color = self.color_active if self.active else self.color_inactive
+        pygame.draw.rect(self.screen, box_color, self.input_box)
         self.screen.blit(txt_surface, (self.input_box.x + 5, self.input_box.y + 5))
-        # Blit the input_box rect.
-        pygame.draw.rect(self.screen, self.color, self.input_box, 2)
+        if self.active and self.cursor_visible:
+            cursor_x = self.input_box.x + txt_surface.get_width() + 5
+            cursor_y = self.input_box.y + 5
+            cursor_rect = pygame.Rect(cursor_x, cursor_y, 2, self.font_small.get_height())
+            pygame.draw.rect(self.screen, self.color, cursor_rect)
 
     def draw_button(self):
-        # Draw the button rect and label
-        pygame.draw.rect(self.screen, self.color_inactive, self.button)
-        button_label = self.font_small.render("Start Game", True, (211, 211, 211))
+        pygame.draw.rect(self.screen, self.button_color, self.button)
+        button_label = self.font_small.render("Start", True, (211, 211, 211))
         self.screen.blit(button_label, (self.button.x + 5, self.button.y + 5))
 
+    def process_start_game(self):
+        self.player_name = self.text if self.text else "PLAYER"
+        self.is_finished = True
+
+    def toggle_cursor(self):
+        if pygame.time.get_ticks() - self.cursor_timer > 500:
+            self.cursor_visible = not self.cursor_visible
+            self.cursor_timer = pygame.time.get_ticks()
+
+    def update(self):
+            self.screen.fill((0, 0, 0))
+            self.render_text()
+            self.position_ui_elements()
+            self.draw_label()
+            self.draw_input_box()
+            self.draw_button()
+            self.toggle_cursor()
+            mouse_pos = pygame.mouse.get_pos()
+            if self.button.collidepoint(mouse_pos):
+                self.button_color = self.color_active
+            else:
+                self.button_color = self.color_inactive
+            pygame.display.flip()
+
+    def handle_event(self, event):
+        if event.type == pygame.KEYDOWN:
+            print(f"Keydown event noticed {event.type}. Inputbox status is {self.active}")
+        
+        if event.type == pygame.KEYDOWN and self.active:
+            print(f"Key pressed: {event.key}") 
+            if event.key == pygame.K_RETURN:
+                self.process_start_game()
+                return
+            elif event.key == pygame.K_BACKSPACE:
+                self.text = self.text[:-1]
+            else:
+                self.text += event.unicode
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print(f"Mouse click at {event.pos}") 
+            if self.input_box.collidepoint(event.pos):
+                if not self.active: 
+                    self.text = ''
+                self.active = True
+            else:
+                self.active = False
+            print(f"Input box active state: {self.active}")
+            if self.button.collidepoint(event.pos) and not self.is_finished:
+                self.process_start_game()
+                return
